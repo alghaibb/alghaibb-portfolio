d0d36a40d71565b05eb5b579ae7d4414ad802f16
Moudi <mjaderi97@gmail.com>
2023-10-25 16:36:22 +1100

    Added dark to light mode theme switcher

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import { Disclosure } from "@headlessui/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-// import Themebutton from "./Themebutton";
+import Themebutton from "./Themebutton";
 
 export default function Navbar() {
   let pathname = usePathname() || "/";
@@ -17,7 +17,7 @@ export default function Navbar() {
                 <div className="flex items-center">
                   <Link href="/">
                     <h1 className="text-2xl font-medium">
-                       <span className="text-teal-500">Mahmoud</span> Jaderi                      
+                      <span className="text-teal-500">Mahmoud</span> Jaderi
                     </h1>
                   </Link>
                 </div>
@@ -53,8 +53,6 @@ export default function Navbar() {
                   >
                     Projects
                   </Link>
-                  {/* <Themebutton /> */}
-                  
                   <Link
                     href="/contact"
                     prefetch
@@ -65,12 +63,12 @@ export default function Navbar() {
                   >
                     Contact
                   </Link>
-                  {/* <Themebutton /> */}
+                  <Themebutton />
                 </div>
               </div>
 
               <div className="-mr-2 flex items-center sm:hidden">
-                {/* <Themebutton /> */}
+                <Themebutton />
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-500 dark:hover:bg-gray-800">
                   {open ? (
                     <svg
