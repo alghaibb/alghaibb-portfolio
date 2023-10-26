f181281a46d58123466c37233c4511878879f727
Moudi <mjaderi97@gmail.com>
2023-10-26 15:15:12 +1100

    attempt 2 to deploy on vercel

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -2,7 +2,7 @@
 
 import { useRef } from "react";
 import { postEntry } from "../action";
-import { experimental_useFormStatus as useFormStatus } from 'react-dom';
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
 
 export default function Form() {
   const formRef = useRef<HTMLFormElement>(null);
@@ -15,20 +15,22 @@ export default function Form() {
       }}
       ref={formRef}
       className="relative flex items-center text-sm mb-5"
-      style={{opacity: pending ? 0.7 : 1}}>
-
+      style={{ opacity: pending ? 0.7 : 1 }}
+    >
       <input
         type="text"
         placeholder="Your message..."
         name="entry"
         required
         disabled={pending}
-        className="pl-4 pr-32 py-2 mt-1 focus:ring-teal-500 focus:border-teal-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100" />
+        className="pl-4 pr-32 py-2 mt-1 focus:ring-teal-500 focus:border-teal-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
+      />
 
       <button
         type="submit"
         disabled={pending}
-        className="flex items-center justify-center absolute right-2 mt-1 font-medium h-7 bg-teal-500/30 text-neutral-900 dark:text-neutral-100 rounded w-16">
+        className="flex items-center justify-center absolute right-2 mt-1 font-medium h-7 bg-teal-500/30 text-neutral-900 dark:text-neutral-100 rounded w-16"
+      >
         Send
       </button>
     </form>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-import Navbar from './components/Navbar'
-import { Provider } from './components/Provider'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Navbar from './components/Navbar';
+import { Provider } from './components/Provider';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Mahmoud Jaderi | My Portfolio',
